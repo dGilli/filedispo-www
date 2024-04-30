17991965a379df1126dbacf99f447749019aa36d
Dennis Gilli <dennis.gilli@acribis.ch>
2024-04-30 18:45:23 +0200

    add new downloads index view

--- src/app.php
+++ src/app.php
@@ -3,9 +3,50 @@
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-return function () {
+$data = [
+    "downloads" => processFiles(
+        "test.pdf",
+        "test.md",
+    ),
+];
+
+function processFiles(...$files) {
+    $processedFiles = [];
+
+    foreach ($files as $file) {
+        $filetype = pathinfo($file, PATHINFO_EXTENSION);
+        $filename = pathinfo($file, PATHINFO_FILENAME) . "." . $filetype;
+        $filesize = filesize($file); // in bytes
+
+        $filesize = formatFileSize($filesize);
+
+        $processedFiles[] = [
+            "filetype" => $filetype,
+            "filename" => $filename,
+            "filesize" => $filesize,
+        ];
+    }
+
+    return $processedFiles;
+}
+
+function formatFileSize($size) {
+    if ($size >= 1024 * 1024 * 1024 * 1024) {
+        return round($size / (1024 * 1024 * 1024 * 1024), 2) . ' TB';
+    } elseif ($size >= 1024 * 1024 * 1024) {
+        return round($size / (1024 * 1024 * 1024), 2) . ' GB';
+    } elseif ($size >= 1024 * 1024) {
+        return round($size / (1024 * 1024), 2) . ' MB';
+    } elseif ($size >= 1024) {
+        return round($size / 1024, 2) . ' KB';
+    } else {
+        return $size . ' bytes';
+    }
+}
+
+return function () use ($data) {
     $loader = new FilesystemLoader(__DIR__ . '/templates');
     $twig = new Environment($loader);
 
-    echo $twig->render('index.twig', ['name' => 'World']);
+    echo $twig->render('index.twig', ['data' => $data]);
 };
